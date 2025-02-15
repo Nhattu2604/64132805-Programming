1017522f5be23d74bb4e117ec95020549f2d5350
Nhattu2604 <nguyennhattucr01ld@gmail.com>
2025-02-16 01:19:43 +0700

    lap 1
    nhập họ và tên và điểm trung bình

---
+++ Lab1_Bai1_DTB/src/DTBSV.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DTBSV {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Nhập họ và tên sinh viên: ");
+     String hoTen = scanner.nextLine();
+
+       
+        System.out.print("Nhập điểm trung bình: ");
+        double diemTB = scanner.nextDouble();
+
+        
+     
+
+	}
+
+}

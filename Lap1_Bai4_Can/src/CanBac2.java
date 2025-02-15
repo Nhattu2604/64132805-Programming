4a62ff8b253bff6246b3dd3d363130fcc6f2eb34
Nhattu2604 <nguyennhattucr01ld@gmail.com>
2025-02-16 02:06:07 +0700

    bài 4, tính căn delta
    đầu tiên nhập hệ số a,b,c

---
+++ Lap1_Bai4_Can/src/CanBac2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CanBac2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Nhập hệ số a, b, c
+        System.out.print("Nhập hệ số a: ");
+        double a = scanner.nextDouble();
+        
+        System.out.print("Nhập hệ số b: ");
+        double b = scanner.nextDouble();
+        
+        System.out.print("Nhập hệ số c: ");
+        double c = scanner.nextDouble();
+
+       
+
+        scanner.close();
+    }
+}

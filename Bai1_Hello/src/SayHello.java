be30e3c97cb4eb0c07e1143da33e2b3789a86c51
Nhattu2604 <nguyennhattucr01ld@gmail.com>
2025-02-16 01:02:29 +0700

    ví dụ hello

---
+++ Bai1_Hello/src/SayHello.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class SayHello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Xin chao . Toi la SV Khoa 64 CNTT: Hoc Lap Trinh Java");
+		System.out.print("Nguyễn Nhật Tú nè");
+
+	}
+
+}

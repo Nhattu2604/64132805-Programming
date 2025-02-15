3880d9e3b6d9b169fb0d73e99d3122379e425dc7
Nhattu2604 <nguyennhattucr01ld@gmail.com>
2025-02-16 01:48:03 +0700

    Update HinhChuNhat.java
    đã xuất màn hình thành công

--- Lap1_Bai2_HinhChuNhat/src/HinhChuNhat.java
+++ Lap1_Bai2_HinhChuNhat/src/HinhChuNhat.java
@@ -12,6 +12,11 @@ public class HinhChuNhat {
 	        double dienTich = chieuDai * chieuRong;
 	        double canhNho = Math.min(chieuDai, chieuRong);
 
+	        // Xuất kết quả
+	        System.out.println("\nThông tin hình chữ nhật:");
+	        System.out.println("Chu vi: " + chuVi);
+	        System.out.println("Diện tích: " + dienTich);
+	        System.out.println("Cạnh nhỏ nhất: " + canhNho);
 
 	        scanner.close();
 	}
